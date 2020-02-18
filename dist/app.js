3f66d20b16569cc217b5609f750471ac0c31b077
Guy Roger Eboulet <eboulet87@gmail.com>
2020-02-18 15:38:55 +0100

    Inserimento testo jumbo

--- dist/app.js
+++ dist/app.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/chiarapassaro/Desktop/boolean/sass-teambit/src/app.js */"./src/app.js");
-module.exports = __webpack_require__(/*! /Users/chiarapassaro/Desktop/boolean/sass-teambit/src/app.scss */"./src/app.scss");
+__webpack_require__(/*! C:\Users\eboul\documents\boolean_careers\esercitazioni\node-sass-teambit\src\app.js */"./src/app.js");
+module.exports = __webpack_require__(/*! C:\Users\eboul\documents\boolean_careers\esercitazioni\node-sass-teambit\src\app.scss */"./src/app.scss");
 
 
 /***/ })
